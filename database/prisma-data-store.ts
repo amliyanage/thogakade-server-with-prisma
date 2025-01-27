8ef57247169f15e250dffce2265b18a26ab07d64
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-01-27 17:09:38 +0530

    Refactor customer management functions and update routes for add, update, delete, and retrieve operations

--- database/prisma-data-store.ts
+++ database/prisma-data-store.ts
@@ -3,7 +3,7 @@ import Customer from "../module/Customer";
 
 const prisma = new PrismaClient()
 
-export async function CustomerAdd(c: Customer) {
+export async function add(c: Customer) {
     try {
          const newCustomer = await prisma.customer.create({
              data:{
@@ -18,5 +18,41 @@ export async function CustomerAdd(c: Customer) {
         return newCustomer
     } catch (err) {
         console.log("error adding customer ", err)
+    }
+}
+
+export async function update(c : Customer) {
+    try{
+        const updateCustomer  = await prisma.customer.update({
+            where: { email : c.email},
+            data : {
+                name : c.name,
+                phone : c.phone
+            }
+        })
+        console.log('customer updated : ', updateCustomer)
+        return updateCustomer
+    } catch ( err ){
+        console.log( 'error updating customer : ' ,err )
+    }
+}
+
+export async function getAllCustomer() {
+    try {
+        return await prisma.customer.findMany()
+    } catch ( err ){
+        console.log( "error deleting : " , err )
+    }
+}
+
+export async function deleteCustomer(email : string){
+    try {
+        const deleteCustomer = await prisma.customer.delete({
+            where : { email : email }
+        })
+        console.log("delete customer " , deleteCustomer )
+        return deleteCustomer
+    } catch ( err ) {
+        console.log(" error deleting customer : " , err)
     }
 }
--- routes/customer-routers.ts
+++ routes/customer-routers.ts
@@ -1,17 +1,49 @@
 import express from "express"
 import Customer from "../module/Customer";
-import {CustomerAdd} from "../database/prisma-data-store";
+import {add, deleteCustomer, getAllCustomer, update} from "../database/prisma-data-store";
 
 const router = express.Router()
 
-router.post("/addCustomer" , async(req,res) => {
+router.post("/add" , async(req,res) => {
     const customer : Customer = req.body
     try {
-        const addedCustomer = await CustomerAdd(customer)
+        const addedCustomer = await add(customer)
         res.status( addedCustomer ? 201 : 400 )
     } catch (err){
         console.log("error adding customer : ",err)
         res.status(400).send("error adding customer")
+    }
+})
+
+router.put('/update',async (req , res ) => {
+    const customer = req.body
+    try{
+        const updatedCustomer = await update(customer)
+        res.status( updatedCustomer ? 201 : 400 )
+    } catch ( err ){
+        console.log("error updating customer : ", err)
+        res.status(400).send("error updating customer")
+    }
+})
+
+router.delete('/delete/:email', async(req , res) => {
+    const email : string = req.params.email
+    try{
+        const deletedCustomer = await deleteCustomer(email)
+        res.status( deletedCustomer ? 201 : 400 )
+    } catch (err){
+        console.log("error deleting customer : ",err)
+        res.status(400).send("error deleting customer")
+    }
+})
+
+router.get('/getAll', async(req , res) => {
+    try{
+        const allCustomers = await getAllCustomer()
+        res.status(201).json(allCustomers)
+    } catch ( err ){
+        console.log("error on get all customer" , err)
+        res.status(400).send("error on get all customer")
     }
 })
 
